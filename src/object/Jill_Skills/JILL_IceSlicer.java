b05400067673b1a7490edddc375cb673b829e21d
japanesebonsai <126158814+japanesebonsai@users.noreply.github.com>
2024-12-02 00:52:32 +0800

    events

--- src/object/Jill_Skills/JILL_IceSlicer.java
+++ src/object/Jill_Skills/JILL_IceSlicer.java
@@ -32,7 +32,7 @@ public class JILL_IceSlicer extends Projectile {
     }
 
     public void getImage() {
-        up1 = setup("/res/projectile/IceSlicer/Icup", gp.tileSize*4, gp.tileSize*4);
+        up1 = setup("/res/projectile/IceSlicer/ICup", gp.tileSize*4, gp.tileSize*4);
         up2 = setup("/res/projectile/IceSlicer/ICup2", gp.tileSize*4, gp.tileSize*4);
         down1 = setup("/res/projectile/IceSlicer/ICdown", gp.tileSize*4, gp.tileSize*4);
         down2 = setup("/res/projectile/IceSlicer/ICdown2", gp.tileSize*4, gp.tileSize*4);
