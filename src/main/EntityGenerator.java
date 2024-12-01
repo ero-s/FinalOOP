737703a5e1cf0321bb78822c099800b4df9c0791
japanesebonsai <126158814+japanesebonsai@users.noreply.github.com>
2024-12-02 01:59:45 +0800

    --

--- src/main/EntityGenerator.java
+++ src/main/EntityGenerator.java
@@ -31,6 +31,7 @@ public class EntityGenerator {
             case OBJ_Fireball.objName : obj = new OBJ_Fireball(gp); break;
             case OBJ_Heart.objName : obj = new OBJ_Heart(gp); break;
             case OBJ_Key.objName : obj = new OBJ_Key(gp); break;
+            case OBJ_Lantern.objName: obj = new OBJ_Lantern(gp); break;
             case OBJ_ManaCrystal.objName : obj = new OBJ_ManaCrystal(gp); break;
             case OBJ_Pickaxe.objName : obj = new OBJ_Pickaxe(gp); break;
             case OBJ_Potion_Red.objName : obj = new OBJ_Potion_Red(gp); break;
@@ -39,8 +40,8 @@ public class EntityGenerator {
             case OBJ_Shield_Blue.objName : obj = new OBJ_Shield_Blue(gp); break;
             case OBJ_Shield_Wood.objName : obj = new OBJ_Shield_Wood(gp); break;
             case OBJ_Sword_Normal.objName : obj = new OBJ_Sword_Normal(gp); break;
-            case OBJ_Lantern.objName: obj = new OBJ_Lantern(gp); break;
             case OBJ_Tent.objName: obj = new OBJ_Tent(gp); break;
+            case OBJ_TrophyJoker.objName : obj = new OBJ_TrophyJoker(gp); break;
             case OBJ_Smash.objName: obj = new OBJ_Smash(gp); break;
             case OBJ_BlueHeart.objName: obj = new OBJ_BlueHeart(gp); break;
             case OBJ_Blizzard.objName: obj = new OBJ_Blizzard(gp);break;
