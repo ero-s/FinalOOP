903713b032b12351ff33f29adaf552f922fc5c29
japanesebonsai <126158814+japanesebonsai@users.noreply.github.com>
2024-12-01 22:11:12 +0800

    i miss her

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -124,11 +124,6 @@ public class AssetSetter {
         gp.monster[mapNum][i].worldY = gp.tileSize * 33;
         i++;
 
-        gp.monster[mapNum][i] = new MON_GreenSlime(gp);
-        gp.monster[mapNum][i].worldX = gp.tileSize * 33;
-        gp.monster[mapNum][i].worldY = gp.tileSize * 15;
-        i++;
-
         gp.monster[mapNum][i] = new MON_Orc(gp);
         gp.monster[mapNum][i].worldX = gp.tileSize * 22;
         gp.monster[mapNum][i].worldY = gp.tileSize * 41;
@@ -137,11 +132,6 @@ public class AssetSetter {
         gp.monster[mapNum][i] = new MON_Orc(gp);
         gp.monster[mapNum][i].worldX = gp.tileSize * 13;
         gp.monster[mapNum][i].worldY = gp.tileSize * 38;
-        i++;
-
-        gp.monster[mapNum][i] = new MON_Orc(gp);
-        gp.monster[mapNum][i].worldX = gp.tileSize * 33;
-        gp.monster[mapNum][i].worldY = gp.tileSize * 15;
         i++;
 
 
@@ -195,11 +185,27 @@ public class AssetSetter {
         gp.monster[mapNum][i].worldY = gp.tileSize * 23;
         i++;
 
+
+        //FALL DUNGEOND
         mapNum = 5;
         i = 0;
         gp.monster[mapNum][i] = new MON_JackOLantern(gp);
         gp.monster[mapNum][i].worldX = gp.tileSize * 25;
         gp.monster[mapNum][i].worldY = gp.tileSize * 16;
+        i++;
+
+
+        //WINTER DUNGEON
+        mapNum = 6;
+        i = 0;
+        gp.monster[mapNum][i] = new MON_Jack(gp);
+        gp.monster[mapNum][i].worldX = gp.tileSize * 20;
+        gp.monster[mapNum][i].worldY = gp.tileSize * 20;
+        i++;
+
+        gp.monster[mapNum][i] = new MON_Jill(gp);
+        gp.monster[mapNum][i].worldX = gp.tileSize * 28;
+        gp.monster[mapNum][i].worldY = gp.tileSize * 20;
         i++;
 
 
